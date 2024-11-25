e86b7d50909e87d9f1f934f8f57d843eea099879
허재 <jea1257@naver.com>
2024-11-25 16:37:54 +0900

    NumberBaseball

--- src/main/java/numberbaseball/Application.java
+++ src/main/java/numberbaseball/Application.java
@@ -1,16 +1,28 @@
 package numberbaseball;
 
+import java.util.Arrays;
 import java.util.Scanner;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class Application {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int cumputer1 = 3;
+        int cumputer2 = 8;
+        int cumputer3 = 1;
+
+
+        boolean isCorrect = false;
+        int strike;
+
         System.out.println("숫자 야구 게임을 시작합니다!");
         System.out.println("1부터 9까지의 서로 다른 숫자 3개를 맞춰보세요.");
 
-        while (true) {
+        while (!(isCorrect)) {
             // 사용자 입력 받기 (이 부분은 완성되어 있음)
             System.out.print("\n첫 번째 숫자를 입력하세요: ");
             int user1 = scanner.nextInt();
@@ -19,13 +31,26 @@ public class Application {
             System.out.print("세 번째 숫자를 입력하세요: ");
             int user3 = scanner.nextInt();
 
-            // TODO: strike 개수를 계산하세요
+            strike = 0;
+            if (user1 == cumputer1) {
+                strike++;
+            }
+            if (user2 == cumputer2) {
+                strike++;
+            }
+            if (user3 == cumputer3) {
+                strike++;;
+            }
+
+            if (strike == 3) {
+                System.out.println("축하합니다. 정답을 맞추셨습니다.");
+                isCorrect = true;
+            }
+            else {
+                System.out.println("스트라이크 " + strike);
+            }
 
 
-            // TODO: 결과를 출력하세요 (예: "1 스트라이크")
-
-
-            // TODO: 3 스트라이크인 경우 게임을 끝내세요
 
 
         }
